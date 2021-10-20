375ff12ba5446eb9effff897ee144e09a30da5c7
Juan Jose <junnior25a@hotmail.com>
2021-10-20 17:32:45 -0500

    Proyecto crono termiado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cronometro';
+  mensaje='';
+
+  actualizar(t:any){
+    this.mensaje=t+'(Se actualiza cada 10 segundos)';
+  }
 }
---
+++ src/app/cronometro/cronometro.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+//import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-cronometro',
+  templateUrl: './cronometro.component.html',
+  styleUrls: ['./cronometro.component.css']
+})
+export class CronometroComponent implements OnInit {
+
+  segundo=0;
+  @Input() inicio!:string;
+  @Output () multiplo10=new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+    this.segundo=Number(this.inicio);
+    setInterval(()=>{
+      this.segundo++;
+      if(this.segundo %10==0) this.multiplo10.emit(this.segundo);
+    },1000);
+  }
+
+}
